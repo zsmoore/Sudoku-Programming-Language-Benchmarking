88ed4d6383a5e22b979eee25093310dc5d06dd41
zsmoore <zsmoore@zachary-moore.com>
2017-08-08 21:41:15 -0700

    Finished C Solver

--- C/solver.c
+++ C/solver.c
@@ -8,9 +8,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+/////////////////////////
+//  Checking
+// Check if number already exists in row
+bool
+inRow(int **grid, int row, int num) {
+    int i;
+    for (i = 0; i < SIZE; i++) {
+        if (grid[row][i] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Check if number already exists in col
+bool
+inCol(int **grid, int col, int num) {
+    int i;
+    for (i = 0; i < SIZE; i++) {
+        if (grid[i][col] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Check if number already exists in Square
+bool
+inSquare(int **grid, int row, int col, int num) {
+    int i, j;
+    for (i = 0; i < (int) sqrt((double) SIZE); i++) {
+        for (j = 0; j < (int) sqrt((double) SIZE); j++) {
+            if (grid[i + row][j + col] == num) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Boolean to check all our checkers
+bool
+safeToPlace(int **grid, int row, int col, int num) {
+    return !inRow(grid, row, num)
+        && !inCol(grid, col, num)
+        && !inSquare(grid,
+                     row - row % 3,
+                     col - col % 3,
+                     num);
+}
+
+
+/////////////////////////
+// Solving
+// Takes in 2d int grid and backtraces until solution or no solution
+bool
+solveSudoku(int **grid) {
+    int row = 0;
+    int col = 0;
+    if (!findEmpty(grid, &row, &col)) {
+        return true;
+    }
+
+    int num;
+    for (num = 1; num < SIZE + 1; num++) {
+        if (safeToPlace(grid, row, col, num)) {
+            grid[row][col] = num;
+            if (solveSudoku(grid)) {
+                return true;
+            }
+            grid[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+// Takes in grid and two int pointer, if grid is solved return false.
+// Else return true and assign int pointers to empty spot
+bool
+findEmpty(int **grid, int *row, int *col) {
+    int i, j;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (grid[i][j] == 0) {
+                *row = i;
+                *col = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 /////////////////////////
 //   File Reading
+// Read in Grid from filename and return in double pointer
 int **
 readGrid(char *filename) {
     
@@ -66,6 +162,7 @@ readGrid(char *filename) {
 
 /////////////////////////
 //  Util
+// Print out 2d grid
 void
 printGrid(int **grid) {
 
@@ -79,6 +176,7 @@ printGrid(int **grid) {
     }
 }
 
+// Free 2d Grid of ints
 void
 freeGrid(int **grid) {
     int i, j;
@@ -98,6 +196,11 @@ main(int argc, char *argv[]) {
     }
     
     int **board = readGrid(argv[1]);
-    printGrid(board);
+    if (solveSudoku(board)) {        
+        printGrid(board);
+    } else {
+        printf("Impossible to solve!\n");
+
+    }
     freeGrid(board);   
 }
--- C/solver.h
+++ C/solver.h
@@ -7,11 +7,21 @@
  *
  */
 
+#include <stdbool.h>
+
 #define SIZE 9
 
 /////////////////////
 //  Checking
+bool inRow(int **, int, int);
+bool inCol(int **, int, int);
+bool inSquare(int **, int, int, int);
+bool safeToPlace(int **, int, int, int);
 
+/////////////////////
+// Solving
+bool solveSudoku(int **);
+bool findEmpty(int **, int *, int *);
 
 /////////////////////
 //  File Reading
